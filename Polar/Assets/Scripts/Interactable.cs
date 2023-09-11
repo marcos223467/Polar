8689288d32939ea44c97a43591f13af67cb004f8
marcos223467 <marcos223467@gmail.com>
2023-09-11 12:43:35 +0200

    Robot interactable

--- Polar/Assets/Scripts/Interactable.cs
+++ Polar/Assets/Scripts/Interactable.cs
@@ -8,11 +8,13 @@ public class Interactable : MonoBehaviour
 {
     [Header("Deteccion")]
     [SerializeField] private Controlador _controlador;
-    [SerializeField] private float minDist;
+    private float minDist, distCuant;
     [SerializeField] private float maxDist;
     
     private Rigidbody _rb;
     private MeshRenderer _meshRenderer;
+    [SerializeField] private SkinnedMeshRenderer _skinnedMesh;
+    public bool skinned;
 
     [SerializeField] private Material[] _materials;
     
@@ -27,9 +29,18 @@ public class Interactable : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();
         polaridad = "";
-        _meshRenderer = GetComponent<MeshRenderer>();
-        _meshRenderer.material = _materials[0];
+        if (!skinned)
+        {
+            _meshRenderer = GetComponent<MeshRenderer>();
+            _meshRenderer.material = _materials[0];
+        }
+        else
+        {
+            _skinnedMesh.material = _materials[0];
+        }
         Fuerza_Magnetica = Vector3.zero;
+        minDist = 0.1f;
+        distCuant = 1000;
     }
 
     void FixedUpdate()
@@ -45,22 +56,38 @@ public class Interactable : MonoBehaviour
         switch (polaridad)
         {
             case "":
-                _meshRenderer.material = _materials[0];
+                if (skinned)
+                    _skinnedMesh.material = _materials[0];
+                else
+                    _meshRenderer.material = _materials[0];
+                
                 _controlador.RemoveSur(gameObject);
                 _controlador.RemoveNorte(gameObject);
                 break;
             case "Norte":
-                _meshRenderer.material = _materials[1];
+                if (skinned)
+                    _skinnedMesh.material = _materials[1];
+                else
+                    _meshRenderer.material = _materials[1];
+                
                 _controlador.RemoveSur(gameObject);
                 _controlador.AddNorte(gameObject);
                 break;
             case "Sur":
-                _meshRenderer.material = _materials[2];
+                if (skinned)
+                    _skinnedMesh.material = _materials[2];
+                else
+                    _meshRenderer.material = _materials[2];
+                
                 _controlador.RemoveNorte(gameObject);
                 _controlador.AddSur(gameObject);
                 break;
             default:
-                _meshRenderer.material = _materials[0];
+                if (skinned)
+                    _skinnedMesh.material = _materials[0];
+                else
+                    _meshRenderer.material = _materials[0];
+                
                 _controlador.RemoveSur(gameObject);
                 _controlador.RemoveNorte(gameObject);
                 break;
@@ -90,16 +117,23 @@ public class Interactable : MonoBehaviour
 
     public void CalculaFuerzaAtraccion(Rigidbody rb)
     {
-        Vector3 direccion = new Vector3(rb.position.x - _rb.position.x, rb.position.y - _rb.position.y,
-            rb.position.z - _rb.position.z).normalized;
-        Fuerza_Magnetica += direccion * (fm / Mathf.Clamp(Vector3.Distance(_rb.position, rb.position), minDist, maxDist));
+        if (Vector3.Distance(_rb.position, rb.position) <= maxDist)
+        {
+            Vector3 direccion = new Vector3(rb.position.x - _rb.position.x, rb.position.y - _rb.position.y,
+                rb.position.z - _rb.position.z).normalized;
+            Fuerza_Magnetica += direccion * (fm / Mathf.Clamp(Vector3.Distance(_rb.position, rb.position), minDist, distCuant));
+        }
     }
     
     public void CalculaFuerzaRepulsion(Rigidbody rb)
     {
-        Vector3 direccion = new Vector3(rb.position.x - _rb.position.x, rb.position.y - _rb.position.y,
-            rb.position.z - _rb.position.z).normalized;
-        Fuerza_Magnetica += -direccion * (fm / Mathf.Clamp(Vector3.Distance(_rb.position, rb.position), minDist, maxDist));
+        if (Vector3.Distance(_rb.position, rb.position) <= maxDist)
+        {
+            Vector3 direccion = new Vector3(rb.position.x - _rb.position.x, rb.position.y - _rb.position.y,
+                rb.position.z - _rb.position.z).normalized;
+            Fuerza_Magnetica += -direccion * (fm / Mathf.Clamp(Vector3.Distance(_rb.position, rb.position), minDist, distCuant));
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
--- Polar/Assets/Scripts/Robot.cs
+++ Polar/Assets/Scripts/Robot.cs
@@ -4,15 +4,98 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
+    [Header("AI detection")]
+    public bool detectado;
+    public LayerMask layer_player, layer_obstaculo;
+    public Transform vision;
+    [Range(0f, 360f)]
+    public float visionAngle;
+    public float visionDistance;
+    
+    [Header("Gizmo Parameters")]
+    public Color seek = Color.green;
+    public Color find = Color.red;
+    public bool mostrar = true;
+
+    [SerializeField] private float vel;
+    [SerializeField] private int vida;
+    
+    public Transform target;
+    private Rigidbody _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        target = GameObject.FindWithTag("Player").transform;
+        detectado = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    private void FixedUpdate()
+    {
+        DetectaJugador();
+
+    }
+    
+    public void DetectaJugador()
+    {
+        Vector3 dist = (target.position - vision.position);
+        float _dist = dist.magnitude;
+        if (Vector3.Angle(dist.normalized, vision.forward) < visionAngle / 2)
+        {
+            if (_dist < visionDistance)
+            {
+                RaycastHit hit;
+                if (!Physics.Raycast(vision.position, target.position - vision.position, out hit, visionDistance, 9) && Physics.Raycast(vision.position, target.position - vision.position, out hit, visionDistance, 8))
+                {
+                    detectado = true;
+                    Debug.DrawRay(vision.position, vision.TransformDirection(Vector3.forward) * hit.distance, Color.blue);
+                }
+                else
+                {
+                    Debug.DrawRay(vision.position, vision.TransformDirection(Vector3.forward) * hit.distance, Color.magenta);
+                    detectado = false;
+                }
+            }
+            else
+            {
+                detectado = false;
+            }
+        }
+        else
+        {
+            detectado = false;
+        }
+    }
+
+    Vector3 PointForAngle(float ang, float dist)
+    {
+        return vision.TransformDirection(new Vector3(Mathf.Sin(ang * Mathf.Deg2Rad), 0, Mathf.Cos(ang * Mathf.Deg2Rad))) * dist;
+    }
+    
+    private void OnDrawGizmos()
+    {
+        if (mostrar)
+        {
+            Gizmos.color = seek;
+            if (detectado)
+                Gizmos.color = find;
+            
+            if (visionAngle >= 0f)
+            {
+                float halfVisionAngle = visionAngle / 2;
+                Vector3 p1, p2;
+                p1 = PointForAngle(halfVisionAngle, visionDistance);
+                p2 = PointForAngle(-halfVisionAngle, visionDistance);
+
+                Gizmos.DrawLine(vision.position, vision.position + p1);
+                Gizmos.DrawLine(vision.position, vision.position + p2);
+            }
+        }
     }
 }
